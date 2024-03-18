29a31161a62198a309acf698c22c138cdd854104
Frozen <71388651+fortex66@users.noreply.github.com>
2024-03-18 15:56:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 332 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,29 @@
+
+while True :
+  a = input()
+  stack = []
+  
+  if a == '.' :
+    break
+  
+  for j in a :
+    if j == '(' or j == '[' :
+      stack.append(j)
+    elif j == ')' :
+      if len(stack) != 0 and stack[-1] == '(':
+        stack.pop()
+      else :
+        stack.append(j)
+        break
+    elif j == ']' :
+      if len(stack) != 0 and stack[-1] == '[':
+        stack.pop()
+      else :
+        stack.append(j)
+        break
+
+   
+  if len(stack) == 0 :
+    print("yes")
+  else :
+    print("no")
