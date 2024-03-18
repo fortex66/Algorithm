b0361f75ae188fa633c8fccf94878918042185ae
Frozen <71388651+fortex66@users.noreply.github.com>
2024-03-18 15:56:17 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(n) :
+  a = input()
+  stack = []
+  for j in a :
+    if j == '(' :
+      stack.append(j)
+    elif j == ')' :
+      if stack :
+        stack.pop()
+      else :
+        print("NO")
+        break
+  else :
+    if not stack :
+      print("YES")
+    else :
+      print("NO")
