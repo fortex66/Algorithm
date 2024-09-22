e105201a19c3a2955461482f67e2791412abf3d4
Frozen <71388651+fortex66@users.noreply.github.com>
2024-09-22 18:04:47 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -6,15 +6,15 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	string s;
 	cin >> s;
 
-	char arr[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	
 	int result[26] = {0,};
+
 	for (int i = 0; i < s.size(); i++) {
-		for (int j = 0; j < sizeof(arr); j++) {
-			if (s[i] == arr[j]) result[j]++;
-		}
+		result[s[i] - 'a']++;
 	}
 
 	for (int i = 0; i < 26; i++) {
