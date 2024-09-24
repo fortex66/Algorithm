1b955d4cdedd26938bdcbb5a77d39f849752a2c7
Frozen <71388651+fortex66@users.noreply.github.com>
2024-09-24 23:58:52 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, K, S, Y, result=0;
+    int arr[15] = {0,};
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> S >> Y;
+        if (S) arr[Y * 2]++;
+        else arr[Y * 2 - 1]++;
+    }
+
+    for (int i = 1; i < 14; i++) {
+        if (arr[i] > 0 && arr[i] <= K) result++;
+        else result += arr[i] / K + arr[i] % K;
+    }
+
+    cout << result;
+
+    return 0;
+}
