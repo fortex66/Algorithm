f342a35df7fe795515d7034efeb1cfe8bff497f3
Frozen <71388651+fortex66@users.noreply.github.com>
2024-09-28 16:51:59 +0900

    [Gold V] Title: 탑, Time: 84 ms, Memory: 8072 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -11,8 +11,8 @@ int main() {
     int N;
     cin >> N;
     vector<int> heights(N + 1); // 탑의 높이를 저장할 벡터
-    stack<int> S;
     vector<int> result(N + 1);  // 결과를 저장할 벡터
+    stack<int> S;
 
     for (int i = 1; i <= N; i++) {
         cin >> heights[i];
