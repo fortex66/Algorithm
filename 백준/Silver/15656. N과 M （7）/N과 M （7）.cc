8092c0d6d295d7b1dafa058a6456e9656ede8262
Frozen <71388651+fortex66@users.noreply.github.com>
2024-10-07 11:56:54 +0900

    [Silver III] Title: N과 M (7), Time: 384 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int ans[10];
+int arr[10]; // 1 7 8 9
+int n, m;
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		ans[k] = arr[i];
+		func(k + 1);
+	}
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	func(0);
+}
