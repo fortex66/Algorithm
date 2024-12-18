78490777510b5bc13f60b995ae6c7088146ab9f3
Frozen <71388651+fortex66@users.noreply.github.com>
2024-12-18 18:12:19 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,28 +5,20 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    // 1. progresses[i] + speeds[i] 해서 100 만들기
-    // 2. progresses[0] == 100 일때 뒤에 progresses들도 검사하기
-    // 3. 뒤에 검사하여 값이 100이면 answer에 개수 push_back()하기
-
     int n = progresses.size();
-    int releaseIndex = 0;
-    
-    while(releaseIndex < n){
+    int idx = 0; // 반복문 시작 
+    // speed의 인덱스와 progress의 인덱스 공유
+    while(1){
+        if(idx==n) break;
+        int cnt = 0;
         for(int i = 0; i < n; i++){
-            progresses[i] = progresses[i] + speeds[i];
+            progresses[i] += speeds[i];
         }
-        int count = 0;
-        while(releaseIndex < n && progresses[releaseIndex] >= 100){
-            count++;
-            releaseIndex++;
+        for(int i = idx; i < n; i++){
+            if(progresses[i]<100) break;
+            else cnt++; idx++;
         }
-        
-        if (count > 0){
-            answer.push_back(count);
-        }
+        if(cnt!=0) answer.push_back(cnt); 
     }
-   
-    
     return answer;
 }
