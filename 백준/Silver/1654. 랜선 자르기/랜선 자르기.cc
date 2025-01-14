2c2cf08af53e0bfbf1b5dfe989a05ebdba64888d
Frozen <71388651+fortex66@users.noreply.github.com>
2025-01-14 22:09:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	// 가지고 있는 랜선의 개수 K
+	// 필요한 랜선의 개수 N
+	ll K, N;
+	cin >> K >> N;
+
+	vector<long long> V;
+	ll st = 1;
+	ll en = 1;
+	ll result = 0;
+	for (int i = 0; i < K; i++) {
+		ll a;
+		cin >> a;
+		V.push_back(a);
+		en = max(en, a);
+	}
+
+	while (st <= en) {
+		ll mid = (st + en) / 2;
+		int cnt = 0;
+		for (int i = 0; i < K; i++) {
+			cnt += V[i] / mid;
+		}
+		if (cnt >= N) {
+			result = mid;
+			st = mid + 1;
+		}
+		else {
+			en = mid - 1;
+		}
+	}
+	
+	cout << result;
+
+}
