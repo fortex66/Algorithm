908514b595104d7eae54576901573f5e6f205e97
Frozen <71388651+fortex66@users.noreply.github.com>
2025-01-31 21:41:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 288 ms, Memory: 10348 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, M;
+	cin >> N;
+
+	vector<int> V1;
+	vector<int> V2;
+	vector<int> ans;
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		V1.push_back(a);
+	}
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		int a;
+		cin >> a;
+		V2.push_back(a);
+	}
+
+	sort(V1.begin(), V1.end());
+
+	for (int i = 0; i < M; i++) {
+		ans.push_back( upper_bound(V1.begin(), V1.end(), V2[i]) - lower_bound(V1.begin(), V1.end(), V2[i]) );
+	}
+
+	for (int i = 0; i < M; i++) {
+		cout << ans[i] << " ";
+	}
+
+	return 0;
+}
