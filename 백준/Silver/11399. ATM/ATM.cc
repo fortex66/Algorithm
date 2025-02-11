2dc74b821ce5e1ee7e2e38fe9c8a24f309e5fae2
Frozen <71388651+fortex66@users.noreply.github.com>
2025-02-11 21:10:43 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -1,28 +1,31 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
-int main(void)
-{
-    int n;
-    int sum = 0;
-    vector<int> arr;
-    cin >> n;
-    
-    for(int i = 0; i<n;i++){
-        int x;
-        cin >> x;
-        arr.push_back(x);
-    }
-    sort(arr.begin(), arr.end());
-    
-    for(int j = 0; j<n;j++){
-        for(int k =0; k<j+1; k++){
-            sum = sum + arr[k];
-        }
-    }
-    cout << sum;
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, ans=0;
+	cin >> n;
+
+	vector<int> v(n,0);
+
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	int prep = 0;
+	for (int i = 0; i < n; i++) {
+		prep += v[i];
+		ans += prep;
+	}
+
+	cout << ans;
+
+	return 0;
 }
